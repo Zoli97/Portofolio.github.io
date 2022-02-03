a1df0424a3e8bd3a424dd2eb26e5a04620b38989
Zoli97 <zzoli97@yahoo.com>
2022-02-03 13:15:02 +0200

    adding some updates to the new branch

---
+++ dist/aboutme.481661b6.js
@@ -0,0 +1,38 @@
+// when the page has completely loaded all the content
+window.onload = ()=>{
+    const tewxtWrap = document.querySelector(".text");
+    tewxtWrap.innerHTML = tewxtWrap.textContent.replace("/S/g", "<span class='letter'>&&</span>");
+    //sync multiple animation on chars in the letter together.
+    anime.timeline({
+        loop: 4000
+    }).add({
+        targets: ".text, .letter",
+        translateX: [
+            40,
+            0
+        ],
+        translateZ: 0,
+        opacity: [
+            0,
+            1
+        ],
+        easing: "easeInExpo",
+        duration: 2200,
+        delay: (el, i)=>500 + 30 * i
+    }).add({
+        targets: ".text, .letter",
+        translateX: [
+            0,
+            -30
+        ],
+        opacity: [
+            1,
+            0
+        ],
+        easing: "easeInExpo",
+        duration: 1500,
+        delay: (el, i)=>100 + 30 * i
+    });
+};
+
+//# sourceMappingURL=aboutme.481661b6.js.map
---
+++ dist/index.9345d665.js
@@ -0,0 +1,21 @@
+//play the lottie when the span is clicked
+const play_button = document.querySelector("button");
+const svg_container = document.getElementById("svg");
+const animation_item = bodymovin.loadAnimation({
+    wrapper: svg_container,
+    animType: "svg",
+    loop: false,
+    autoplay: false,
+    path: "https://assets8.lottiefiles.com/packages/lf20_tkvgymkx.json"
+});
+play_button.addEventListener("click", ()=>{
+    //not hidden anymore
+    svg_container.classList.remove("hide");
+    animation_item.goToAndPlay(0, true);
+});
+animation_item.addEventListener("complete", ()=>{
+    //once the aniamtion is complete
+    svg_container.classList.add("hide");
+});
+
+//# sourceMappingURL=index.9345d665.js.map
