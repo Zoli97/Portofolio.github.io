30e2c13d7ce26387944230064fa008a15d4b3855
Zoli97 <zzoli97@yahoo.com>
2023-05-19 11:35:17 +0300

    change the script for the slider

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-import Glide from "@glidejs/glide";
-
 //play the lottie when the span is clicked
 const play_button = document.querySelector("button");
 const svg_container = document.getElementById("svg");
@@ -21,16 +19,3 @@ animation_item.addEventListener("complete", () => {
   //once the aniamtion is complete
   svg_container.classList.add("hide");
 });
-
-new Glide(".glide", {
-  type: "slider",
-  perView: 3,
-  breakpoints: {
-    600: { perView: 1 },
-    800: { perView: 2 },
-    950: { perView: 3 },
-    1500: { perView: 4 },
-  },
-  autoplay: 2500,
-  bound: true,
-}).mount();
