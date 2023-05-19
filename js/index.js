d721bc06a20ff304fdacc06b7fd3b479d49a4135
Zoli97 <zzoli97@yahoo.com>
2023-05-19 11:21:33 +0300

    adding photos

--- js/index.js
+++ js/index.js
@@ -1,21 +1,36 @@
+import Glide from "@glidejs/glide";
+
 //play the lottie when the span is clicked
 const play_button = document.querySelector("button");
 const svg_container = document.getElementById("svg");
 const animation_item = bodymovin.loadAnimation({
-    wrapper: svg_container,
-    animType: "svg",
-    loop: false,
-    autoplay: false,
-    path: "https://assets8.lottiefiles.com/packages/lf20_tkvgymkx.json",
+  wrapper: svg_container,
+  animType: "svg",
+  loop: false,
+  autoplay: false,
+  path: "https://assets8.lottiefiles.com/packages/lf20_tkvgymkx.json",
 });
 
 play_button.addEventListener("click", () => {
-    //not hidden anymore
-    svg_container.classList.remove("hide");
-    animation_item.goToAndPlay(0, true);
+  //not hidden anymore
+  svg_container.classList.remove("hide");
+  animation_item.goToAndPlay(0, true);
 });
 
 animation_item.addEventListener("complete", () => {
-    //once the aniamtion is complete
-    svg_container.classList.add("hide");
+  //once the aniamtion is complete
+  svg_container.classList.add("hide");
 });
+
+new Glide(".glide", {
+  type: "slider",
+  perView: 3,
+  breakpoints: {
+    600: { perView: 1 },
+    800: { perView: 2 },
+    950: { perView: 3 },
+    1500: { perView: 4 },
+  },
+  autoplay: 2500,
+  bound: true,
+}).mount();
