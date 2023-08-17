381376df6855b8c54edd0880f4fb525fbfdd0fad
Zoli97 <zzoli97@yahoo.com>
2023-08-17 11:39:05 +0300

    making some changes

--- dist/contact.be2ce8a8.js
+++ dist/contact.be2ce8a8.js
@@ -11,5 +11,37 @@ hamburger.addEventListener("click", ()=>{
         nav.classList.toggle("fade");
     });
 });
+//fadein animation
+const $ = function(selector) {
+    const html_elements = document.querySelectorAll.call(document, selector);
+    //actual library functionality(elements that i want to animate)
+    const fade_library = {
+        html_elements,
+        addClass (clasName) {
+            this.html_elements.forEach((html_element)=>{
+                html_element.classList.add(clasName);
+            });
+            return fade_library;
+        },
+        fadeIn (dur) {
+            this.html_elements.forEach((html_elem)=>{
+                let currentOpacity = 0;
+                const increment = 1 / (dur / 10);
+                const fadeIn = setInterval(()=>{
+                    currentOpacity += increment;
+                    html_elem.style.opacity = currentOpacity;
+                    //reach 0
+                    if (currentOpacity <= 0) {
+                        clearInterval(fadeIn);
+                        html_elem.style.display = "none";
+                    }
+                }, 10);
+            });
+            return fade_library;
+        }
+    };
+    return fade_library;
+};
+$(".fadein").addClass("hide_logo").fadeIn(3500);
 
 //# sourceMappingURL=contact.be2ce8a8.js.map
--- js/parallax.js
+++
@@ -1,7 +0,0 @@
-// //import Rellax from "rellax";
-// import Rellax from "rellax";
-// var rellax = new Rellax(".rellax", {
-//   center: true,
-//   breakpoints: [576, 768, 1150, 1201, 1250, 1550],
-//   offset: true,
-// });
