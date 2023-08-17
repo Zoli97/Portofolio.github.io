6733aafcc764454ecce739d2bfef72dc3b0edd20
Zoli97 <zzoli97@yahoo.com>
2023-08-17 11:39:05 +0300

    making some changes

--- js/hamburger.js
+++ js/hamburger.js
@@ -8,10 +8,49 @@ const nav_links = document.querySelectorAll(".nav-menu li ");
 //function on the hamburger (add this active class which makes open the hamburger menu, and with .active class i can create the animation when i click on it.);
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 
-    nav_links.forEach((nav) => {
-        nav.classList.toggle("fade");
-    });
+  nav_links.forEach((nav) => {
+    nav.classList.toggle("fade");
+  });
 });
+
+//fadein animation
+
+const $ = function (selector) {
+  const html_elements = document.querySelectorAll.call(document, selector);
+  //actual library functionality(elements that i want to animate)
+
+  const fade_library = {
+    html_elements,
+    addClass(clasName) {
+      this.html_elements.forEach((html_element) => {
+        html_element.classList.add(clasName);
+      });
+      return fade_library;
+    },
+
+    fadeIn(dur) {
+      this.html_elements.forEach((html_elem) => {
+        let currentOpacity = 0;
+        const increment = 1 / (dur / 10);
+
+        const fadeIn = setInterval(() => {
+          currentOpacity += increment;
+          html_elem.style.opacity = currentOpacity;
+
+          //reach 0
+          if (currentOpacity <= 0) {
+            clearInterval(fadeIn);
+            html_elem.style.display = "none";
+          }
+        }, 10);
+      });
+      return fade_library;
+    },
+  };
+
+  return fade_library;
+};
+$(".fadein").addClass("hide_logo").fadeIn(3500);
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 //play the lottie when the span is clicked
 const play_button = document.querySelector("button");
 const svg_container = document.getElementById("svg");
+
 const animation_item = bodymovin.loadAnimation({
   wrapper: svg_container,
   animType: "svg",
@@ -19,3 +20,42 @@ animation_item.addEventListener("complete", () => {
   //once the aniamtion is complete
   svg_container.classList.add("hide");
 });
+
+//fadein animation
+
+const $ = function (selector) {
+  const html_elements = document.querySelectorAll.call(document, selector);
+  //actual library functionality(elements that i want to animate)
+
+  const fade_library = {
+    html_elements,
+    addClass(clasName) {
+      this.html_elements.forEach((html_element) => {
+        html_element.classList.add(clasName);
+      });
+      return fade_library;
+    },
+
+    fadeIn(dur) {
+      this.html_elements.forEach((html_elem) => {
+        let currentOpacity = 0;
+        const increment = 1 / (dur / 10);
+
+        const fadeIn = setInterval(() => {
+          currentOpacity += increment;
+          html_elem.style.opacity = currentOpacity;
+
+          //reach 0
+          if (currentOpacity <= 0) {
+            clearInterval(fadeIn);
+            html_elem.style.display = "none";
+          }
+        }, 10);
+      });
+      return fade_library;
+    },
+  };
+
+  return fade_library;
+};
+$(".fadein").addClass("hide_logo").fadeIn(3500);
