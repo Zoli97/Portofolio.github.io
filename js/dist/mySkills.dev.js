c6d5e792061fed4732a25dd885430edec3fe0489
Zoli97 <zzoli97@yahoo.com>
2026-09-18 16:43:43 +0300

    make some new changes

--- js/dist/mySkills.dev.js
+++ js/dist/mySkills.dev.js
@@ -3,79 +3,196 @@
 //get the elements from DOM
 var button_ = document.querySelector("#btn_");
 var arrow_btn = document.querySelector("#arrow-btn");
-var image_ = document.querySelector("#image"); //create a function to add a new section item to my sections list.
-//array of object as param.
+var image_ = document.querySelector("#image");
+var hr = document.querySelector(".hr");
+var links = [{
+  text: "All",
+  filter_category: "all"
+}, {
+  text: "Marvel",
+  filter_category: "Marvel"
+}, {
+  text: "DC",
+  filter_category: "DC"
+}];
+var imageData = [{
+  src: "./images/marvel/thor.jpeg",
+  alt: "thor",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/spiderman.jpeg",
+  alt: "spider-man",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/doom.jpeg",
+  alt: "doom",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/hulk.jpeg",
+  alt: "hulk",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/drstrange.jpeg",
+  alt: "dr-strange",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/ironman.jpeg",
+  alt: "iron-manss",
+  category: "Marvel"
+}, {
+  src: "./images/dc/cap-america.jpeg",
+  alt: "cpt-america",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/venom.jpeg",
+  alt: "venom",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/carnage.jpeg",
+  alt: "carnage",
+  category: "Marvel"
+}, {
+  src: "./images/marvel/black-cat.jpeg",
+  alt: "black-cat",
+  category: "Marvel"
+}, {
+  src: "./images/dc/joker.jpeg",
+  alt: "joker",
+  category: "DC"
+}, {
+  src: "./images/dc/batman.jpeg",
+  alt: "batman",
+  category: "DC"
+}, {
+  src: "./images/dc/wonder_woman.jpeg",
+  alt: "wonder-soman",
+  category: "DC"
+}, {
+  src: "./images/dc/flash.jpeg",
+  alt: "flash",
+  category: "DC"
+}, {
+  src: "./images/dc/aquaman.jpeg",
+  alt: "aquaman",
+  category: "DC"
+}, {
+  src: "./images/dc/pink.jpeg",
+  alt: "pink",
+  category: "DC"
+}, {
+  src: "./images/dc/robin.jpeg",
+  alt: "robin",
+  category: "DC"
+}, {
+  src: "./images/dc/superman.jpeg",
+  alt: "superman",
+  category: "DC"
+}, {
+  src: "./images/dc/greenlight.jpeg",
+  alt: "greenlight",
+  category: "DC"
+}, {
+  src: "./images/dc/kovacs.jpeg",
+  alt: "kovacs",
+  category: "DC"
+}];
 
-function createNewSectionItem(images) {
-  var section = document.createElement("section");
-  section.className = "section4";
-  var article = createArticle("article4", "lightgallery", images);
-  section.appendChild(article);
-  return section;
+function createNavbar(links) {
+  var nav = document.createElement("nav");
+  var ul = document.createElement("ul"); //loop through the links
+
+  links.forEach(function (_ref) {
+    var text = _ref.text,
+        filter_category = _ref.filter_category;
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.textContent = text;
+    li.setAttribute("data-filter", filter_category);
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+  nav.appendChild(ul);
+  return nav;
 }
 
-function createImage(classes, src, alt) {
-  var image = document.createElement("img");
-  image.src = src;
-  image.className = classes;
-  image.alt = alt;
-  return image;
-} //Function that creates the article element and appends the images to it.
-
-
-function createArticle(classes, id, images) {
-  var article = document.createElement("article");
-  article.setAttribute("id", id);
-  article.className = classes; //Iterates through each object in the 'images' array using property destructuring ({ classes, src, alt })
-  //foreach -because sarticle contains 3 different images.
-
-  images.forEach(function (_ref) {
-    var classes = _ref.classes,
-        src = _ref.src,
-        alt = _ref.alt;
-    var image = createImage(classes, src, alt);
-    article.appendChild(image);
+function styleNavbar(nav) {
+  var ul = nav.querySelector("ul");
+  var li = ul.querySelectorAll("li");
+  nav.style.width = "100%";
+  ul.classList.add("nav-link");
+  li.forEach(function (item) {
+    var a = item.querySelector("a");
+    item.style.fontSize = "20px";
+    item.style.padding = "1.2rem";
+    a.style.cursor = "pointer";
   });
-  return article;
-} //an array of objects with the props like class, src, alt, each object is an img item containing the next props.
+}
+
+var nav = createNavbar(links);
+styleNavbar(nav);
+hr.insertAdjacentElement("afterend", nav); //create gallery
+
+function createGallery(imgs) {
+  var container = document.createElement("div");
+  container.classList.add("gallery"); //loop through the images array of objects, destructure the props
+
+  imgs.forEach(function (_ref2) {
+    var src = _ref2.src,
+        alt = _ref2.alt,
+        category = _ref2.category;
+    var image = document.createElement("img");
+    image.src = src; //set the img src
+
+    image.alt = alt; //set the alt text
+
+    image.setAttribute("data-category", category); // tag the img its category as a data-attr
+
+    image.classList.add("img");
+    container.appendChild(image);
+  });
+  return container;
+} //click event on li
 
 
-var images = [{
-  classes: "card-img",
-  src: "./images/me3.jpg",
-  alt: "pic"
-}, {
-  classes: "card-img1",
-  src: "./images/me4.jpg",
-  alt: "pic"
-}, {
-  classes: "card-img2",
-  src: "./images/me5.jpg",
-  alt: "pic"
-}];
-var newSection = createNewSectionItem(images);
-document.querySelector("main").appendChild(newSection);
-var rellax = new Rellax(".rellax", {
-  center: true,
-  breakpoints: [576, 768, 1150, 1201, 1250, 1550],
-  offset: true
-});
-ScrollOut({
-  threshold: 0.5,
-  targets: ".img, .my-text, .card-img, .card-img1, .card-img2",
-  observeChanges: true
-});
-var light_gallery = document.getElementById("lightgallery");
-lightGallery(light_gallery, {
-  speed: 400,
-  mode: "lg-fade",
-  showMaximizeIcon: true
-});
+function filterEvent(nav) {
+  var liList = nav.querySelectorAll("li");
+  liList.forEach(function (li) {
+    //add event listener on li
+    li.addEventListener("click", function () {
+      //read the data-filter attr stored on the li
+      var category = li.getAttribute("data-filter");
+      filterImages(category); //toggle active state (only one li has .active at a time)
+
+      liList.forEach(function (item) {
+        return item.classList.remove("active");
+      });
+      li.classList.add("active");
+    });
+  });
+} //filterImages show or hides imgs based on matching data-category attribute.
+
+
+function filterImages(category) {
+  var images = document.querySelectorAll("[data-category]");
+  images.forEach(function (img) {
+    var matches = category == "all" || img.dataset.category === category; //or click a specific category
+
+    img.style.display = matches ? "" : "none";
+  });
+} //create the gallery with the img data [{}]
+
+
+var gallery = createGallery(imageData);
+nav.insertAdjacentElement("afterend", gallery);
+filterEvent(nav);
 var docWidth = document.documentElement.offsetWidth;
 [].forEach.call(document.querySelectorAll("*"), function (el) {
   if (el.offsetWidth > docWidth) {
     console.log(el);
   }
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var rellax = new Rellax(".rellax");
 }); //need to know how tall is the navbar, so now i can use this now to update or apply to html
 
 var scrollHeight = document.querySelector(".nav-container").offsetHeight; //set custom prop
@@ -87,29 +204,14 @@ var resetItems = function resetItems() {
   items.forEach(function (item) {
     return item.classList.remove("active");
   });
-};
-
-var handleClick = function handleClick(element, index) {
-  resetItems();
-  element.classList.add("active");
-  localStorage.setItem("accordionIndex", index);
-};
-
-resetItems();
-var activeItem = items[localStorage.getItem("accordionIndex")];
-
-if (activeItem) {
-  activeItem.classList.add("active");
-} else {
-  items[0].classList.add("active");
-} //buttons selected
+}; //buttons selected
 
 
 var handleButtonClick = function handleButtonClick(e) {
   e.stopPropagation();
   e.currentTarget.classList.toggle("active-btn");
-  image_.classList.add("glitch-active");
-  console.log(button_); //remove the glitch when animation end
+  image_.classList.add("glitch-active"); // console.log(button_);
+  //remove the glitch when animation end
 
   setTimeout(function () {
     image_.classList.remove("glitch-active");
--- js/mySkills.js
+++ js/mySkills.js
@@ -2,68 +2,150 @@
 const button_ = document.querySelector("#btn_");
 const arrow_btn = document.querySelector("#arrow-btn");
 const image_ = document.querySelector("#image");
+const hr = document.querySelector(".hr");
 
-//create a function to add a new section item to my sections list.
-//array of object as param.
-
-function createNewSectionItem(images) {
-  const section = document.createElement("section");
-  section.className = "section4";
-  const article = createArticle("article4", "lightgallery", images);
-  section.appendChild(article);
-  return section;
-}
-
-function createImage(classes, src, alt) {
-  const image = document.createElement("img");
-  image.src = src;
-  image.className = classes;
-  image.alt = alt;
-
-  return image;
-}
-
-//Function that creates the article element and appends the images to it.
-function createArticle(classes, id, images) {
-  const article = document.createElement("article");
-  article.setAttribute("id", id);
-  article.className = classes;
-
-  //Iterates through each object in the 'images' array using property destructuring ({ classes, src, alt })
-  //foreach -because sarticle contains 3 different images.
-  images.forEach(({ classes, src, alt }) => {
-    const image = createImage(classes, src, alt);
-    article.appendChild(image);
-  });
-  return article;
-}
-
-//an array of objects with the props like class, src, alt, each object is an img item containing the next props.
-const images = [
-  { classes: "card-img", src: "./images/me3.jpg", alt: "pic" },
-  { classes: "card-img1", src: "./images/me4.jpg", alt: "pic" },
-  { classes: "card-img2", src: "./images/me5.jpg", alt: "pic" },
+const links = [
+  { text: "All", filter_category: "all" },
+  { text: "Marvel", filter_category: "Marvel" },
+  { text: "DC", filter_category: "DC" },
 ];
 
-const newSection = createNewSectionItem(images);
-document.querySelector("main").appendChild(newSection);
+const imageData = [
+  { src: "./images/marvel/thor.jpeg", alt: "thor", category: "Marvel" },
+  {
+    src: "./images/marvel/spiderman.jpeg",
+    alt: "spider-man",
+    category: "Marvel",
+  },
+  { src: "./images/marvel/doom.jpeg", alt: "doom", category: "Marvel" },
+  { src: "./images/marvel/hulk.jpeg", alt: "hulk", category: "Marvel" },
 
-const rellax = new Rellax(".rellax", {
-  center: true,
-  breakpoints: [576, 768, 1150, 1201, 1250, 1550],
-  offset: true,
-});
-ScrollOut({
-  threshold: 0.5,
-  targets: ".img, .my-text, .card-img, .card-img1, .card-img2",
-  observeChanges: true,
-});
-const light_gallery = document.getElementById("lightgallery");
-lightGallery(light_gallery, {
-  speed: 400,
-  mode: "lg-fade",
-  showMaximizeIcon: true,
-});
+  {
+    src: "./images/marvel/drstrange.jpeg",
+    alt: "dr-strange",
+    category: "Marvel",
+  },
+  {
+    src: "./images/marvel/ironman.jpeg",
+    alt: "iron-manss",
+    category: "Marvel",
+  },
+  {
+    src: "./images/dc/cap-america.jpeg",
+    alt: "cpt-america",
+    category: "Marvel",
+  },
+  { src: "./images/marvel/venom.jpeg", alt: "venom", category: "Marvel" },
+  { src: "./images/marvel/carnage.jpeg", alt: "carnage", category: "Marvel" },
+  {
+    src: "./images/marvel/black-cat.jpeg",
+    alt: "black-cat",
+    category: "Marvel",
+  },
+  { src: "./images/dc/joker.jpeg", alt: "joker", category: "DC" },
+
+  { src: "./images/dc/batman.jpeg", alt: "batman", category: "DC" },
+  {
+    src: "./images/dc/wonder_woman.jpeg",
+    alt: "wonder-soman",
+    category: "DC",
+  },
+  { src: "./images/dc/flash.jpeg", alt: "flash", category: "DC" },
+  { src: "./images/dc/aquaman.jpeg", alt: "aquaman", category: "DC" },
+  { src: "./images/dc/pink.jpeg", alt: "pink", category: "DC" },
+  { src: "./images/dc/robin.jpeg", alt: "robin", category: "DC" },
+  { src: "./images/dc/superman.jpeg", alt: "superman", category: "DC" },
+  { src: "./images/dc/greenlight.jpeg", alt: "greenlight", category: "DC" },
+  { src: "./images/dc/kovacs.jpeg", alt: "kovacs", category: "DC" },
+];
+
+function createNavbar(links) {
+  const nav = document.createElement("nav");
+  const ul = document.createElement("ul");
+
+  //loop through the links
+
+  links.forEach(({ text, filter_category }) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.textContent = text;
+    li.setAttribute("data-filter", filter_category);
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+  nav.appendChild(ul);
+  return nav;
+}
+
+function styleNavbar(nav) {
+  const ul = nav.querySelector("ul");
+  const li = ul.querySelectorAll("li");
+
+  nav.style.width = "100%";
+  ul.classList.add("nav-link");
+
+  li.forEach((item) => {
+    const a = item.querySelector("a");
+    item.style.fontSize = "20px";
+    item.style.padding = "1.2rem";
+    a.style.cursor = "pointer";
+  });
+}
+
+const nav = createNavbar(links);
+styleNavbar(nav);
+
+hr.insertAdjacentElement("afterend", nav);
+
+//create gallery
+function createGallery(imgs) {
+  const container = document.createElement("div");
+  container.classList.add("gallery");
+
+  //loop through the images array of objects, destructure the props
+  imgs.forEach(({ src, alt, category }) => {
+    const image = document.createElement("img");
+    image.src = src; //set the img src
+    image.alt = alt; //set the alt text
+    image.setAttribute("data-category", category); // tag the img its category as a data-attr
+    image.classList.add("img");
+    container.appendChild(image);
+  });
+
+  return container;
+}
+
+//click event on li
+function filterEvent(nav) {
+  const liList = nav.querySelectorAll("li");
+
+  liList.forEach((li) => {
+    //add event listener on li
+    li.addEventListener("click", () => {
+      //read the data-filter attr stored on the li
+      const category = li.getAttribute("data-filter");
+      filterImages(category);
+
+      //toggle active state (only one li has .active at a time)
+      liList.forEach((item) => item.classList.remove("active"));
+      li.classList.add("active");
+    });
+  });
+}
+
+//filterImages show or hides imgs based on matching data-category attribute.
+function filterImages(category) {
+  const images = document.querySelectorAll("[data-category]");
+  images.forEach((img) => {
+    const matches = category == "all" || img.dataset.category === category; //or click a specific category
+    img.style.display = matches ? "" : "none";
+  });
+}
+
+//create the gallery with the img data [{}]
+const gallery = createGallery(imageData);
+nav.insertAdjacentElement("afterend", gallery);
+filterEvent(nav);
 
 var docWidth = document.documentElement.offsetWidth;
 
@@ -71,6 +153,10 @@ var docWidth = document.documentElement.offsetWidth;
   if (el.offsetWidth > docWidth) {
     console.log(el);
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  var rellax = new Rellax(".rellax");
 });
 
 //need to know how tall is the navbar, so now i can use this now to update or apply to html
@@ -85,27 +171,13 @@ const resetItems = () => {
   items.forEach((item) => item.classList.remove("active"));
 };
 
-const handleClick = (element, index) => {
-  resetItems();
-  element.classList.add("active");
-  localStorage.setItem("accordionIndex", index);
-};
-resetItems();
-
-const activeItem = items[localStorage.getItem("accordionIndex")];
-if (activeItem) {
-  activeItem.classList.add("active");
-} else {
-  items[0].classList.add("active");
-}
-
 //buttons selected
 const handleButtonClick = (e) => {
   e.stopPropagation();
   e.currentTarget.classList.toggle("active-btn");
   image_.classList.add("glitch-active");
 
-  console.log(button_);
+  // console.log(button_);
   //remove the glitch when animation end
   setTimeout(() => {
     image_.classList.remove("glitch-active");
